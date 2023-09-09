a16d63950ea20941190bc1cee9abce3724975059
satheeshpolu <satheeshpoluab@gmail.com>
2023-09-09 22:45:34 +0200

    fix: restructured project

---
+++ src/rest-api/api.js
@@ -0,0 +1,10 @@
+const API = {
+    ROOT: '/api/v1',
+    ALL_NOTES: '/all-notes',
+    ADD_NOTE: '/add-note',
+    DELETE_NOTE: '/delete-note',
+    UPDATE_NOTE: '/update-note',
+    DELETE_ALL_NOTES: '/delete-all-notes',
+};
+
+module.exports = API;
