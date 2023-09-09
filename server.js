514ca1e0623755c40b2b21b1c46e687808df6236
satheeshpolu <satheeshpoluab@gmail.com>
2023-09-09 13:07:49 +0200

    feat: first REST API configured

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const API_PORT = 5000;
 const app = express();
@@ -7,4 +7,11 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-app.listen(API_PORT, () => console.log(`BE: Server is running on ${API_PORT}`));
+app.get("/api", async function (req, res) {
+  res.send({ result: "api test" });
+});
+
+app.listen(API_PORT, () =>
+  console.log(`Server is running on ${API_PORT} 
+    \n visit http://localhost:5000/api`)
+);
